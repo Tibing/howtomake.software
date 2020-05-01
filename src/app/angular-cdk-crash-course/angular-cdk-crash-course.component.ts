f8e72b2527cdd79272c9538141c8d7be80061373
Tibing <nik.poltoratsky@gmail.com>
2020-05-02 02:14:21 +0300

    fix: styles

--- src/app/angular-cdk-crash-course/angular-cdk-crash-course.component.ts
+++ src/app/angular-cdk-crash-course/angular-cdk-crash-course.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-angular-cdk-crash-course',
@@ -6,8 +7,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./angular-cdk-crash-course.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AngularCdkCrashCourseComponent {
+export class AngularCdkCrashCourseComponent implements OnInit, OnDestroy {
   drag = false;
   overlays = false;
   tables = false;
+
+  private prevBackground: string;
+
+  constructor(@Inject(DOCUMENT) private document) {
+  }
+
+  ngOnDestroy(): void {
+    this.document.documentElement.style.backgroundColor = this.prevBackground;
+  }
+
+  ngOnInit(): void {
+    this.prevBackground = this.document.documentElement.style.backgroundColor;
+    this.document.documentElement.style.backgroundColor = 'white';
+  }
 }
