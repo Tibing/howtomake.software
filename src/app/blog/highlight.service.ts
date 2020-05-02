a11b0e9ad6dd935116fccdab471868af67ecdb28
Tibing <nik.poltoratsky@gmail.com>
2020-05-02 17:56:46 +0300

    fix: code highlight

--- src/app/blog/highlight.service.ts
+++ src/app/blog/highlight.service.ts
@@ -1,6 +1,17 @@
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
+import 'clipboard/dist/clipboard.js';
+import 'prismjs/prism.js';
+import 'prismjs/plugins/toolbar/prism-toolbar.js';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js';
+import 'prismjs/components/prism-bash.js';
+import 'prismjs/components/prism-css.js';
+import 'prismjs/components/prism-javascript.js';
+import 'prismjs/components/prism-json.js';
+import 'prismjs/components/prism-markup.js';
+import 'prismjs/components/prism-typescript.js';
+
 
 declare var Prism: {
   highlightAll(): void;
